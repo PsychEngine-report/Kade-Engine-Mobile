472eea7f553f409e1bb1b8787776dbcdc3506367
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-10-02 23:13:56 +0200

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,1 +1,11 @@
 import Paths;
+import mobile.StorageUtil;
+#if android
+import android.content.Context as AndroidContext;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
